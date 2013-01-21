7ec4f64cf35c0c97183c946e176fc902304d459c
Eko Suprapto Wibowo <swdev.bali@gmail.com>
2013-01-21 08:09:59 +0700

    using POP.NET
    able to fetch email using my pop3 email at swdevbali

--- SupportAlerter/SupportAlerter.Designer.cs
+++ SupportAlerter/SupportAlerter.Designer.cs
@@ -29,22 +29,360 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(SupportAlerter));
+            this.panelTop = new System.Windows.Forms.Panel();
+            this.useSslCheckBox = new System.Windows.Forms.CheckBox();
+            this.uidlButton = new System.Windows.Forms.Button();
+            this.totalMessagesTextBox = new System.Windows.Forms.TextBox();
+            this.labelTotalMessages = new System.Windows.Forms.Label();
+            this.labelPassword = new System.Windows.Forms.Label();
+            this.passwordTextBox = new System.Windows.Forms.TextBox();
+            this.labelUsername = new System.Windows.Forms.Label();
+            this.loginTextBox = new System.Windows.Forms.TextBox();
+            this.connectAndRetrieveButton = new System.Windows.Forms.Button();
+            this.labelServerPort = new System.Windows.Forms.Label();
+            this.portTextBox = new System.Windows.Forms.TextBox();
+            this.labelServerAddress = new System.Windows.Forms.Label();
+            this.popServerTextBox = new System.Windows.Forms.TextBox();
+            this.panelMiddle = new System.Windows.Forms.Panel();
+            this.panelMessageBody = new System.Windows.Forms.Panel();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.messageTextBox = new System.Windows.Forms.TextBox();
+            this.labelMessageBody = new System.Windows.Forms.Label();
+            this.panelMessagesView = new System.Windows.Forms.Panel();
+            this.listMessages = new System.Windows.Forms.TreeView();
+            this.labelMessageNumber = new System.Windows.Forms.Label();
+            this.attachmentPanel = new System.Windows.Forms.Panel();
+            this.listAttachments = new System.Windows.Forms.TreeView();
+            this.labelAttachments = new System.Windows.Forms.Label();
+            this.panelProperties = new System.Windows.Forms.Panel();
+            this.gridHeaders = new System.Windows.Forms.DataGrid();
+            this.panelTop.SuspendLayout();
+            this.panelMiddle.SuspendLayout();
+            this.panelMessageBody.SuspendLayout();
+            this.panelMessagesView.SuspendLayout();
+            this.attachmentPanel.SuspendLayout();
+            this.panelProperties.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.gridHeaders)).BeginInit();
             this.SuspendLayout();
+            // 
+            // panelTop
+            // 
+            this.panelTop.Controls.Add(this.useSslCheckBox);
+            this.panelTop.Controls.Add(this.uidlButton);
+            this.panelTop.Controls.Add(this.totalMessagesTextBox);
+            this.panelTop.Controls.Add(this.labelTotalMessages);
+            this.panelTop.Controls.Add(this.labelPassword);
+            this.panelTop.Controls.Add(this.passwordTextBox);
+            this.panelTop.Controls.Add(this.labelUsername);
+            this.panelTop.Controls.Add(this.loginTextBox);
+            this.panelTop.Controls.Add(this.connectAndRetrieveButton);
+            this.panelTop.Controls.Add(this.labelServerPort);
+            this.panelTop.Controls.Add(this.portTextBox);
+            this.panelTop.Controls.Add(this.labelServerAddress);
+            this.panelTop.Controls.Add(this.popServerTextBox);
+            this.panelTop.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelTop.Location = new System.Drawing.Point(0, 0);
+            this.panelTop.Name = "panelTop";
+            this.panelTop.Size = new System.Drawing.Size(806, 64);
+            this.panelTop.TabIndex = 1;
+            // 
+            // useSslCheckBox
+            // 
+            this.useSslCheckBox.AutoSize = true;
+            this.useSslCheckBox.Location = new System.Drawing.Point(19, 38);
+            this.useSslCheckBox.Name = "useSslCheckBox";
+            this.useSslCheckBox.Size = new System.Drawing.Size(68, 17);
+            this.useSslCheckBox.TabIndex = 4;
+            this.useSslCheckBox.Text = "Use SSL";
+            this.useSslCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // uidlButton
+            // 
+            this.uidlButton.Enabled = false;
+            this.uidlButton.Location = new System.Drawing.Point(460, 42);
+            this.uidlButton.Name = "uidlButton";
+            this.uidlButton.Size = new System.Drawing.Size(82, 21);
+            this.uidlButton.TabIndex = 6;
+            this.uidlButton.Text = "UIDL";
+            // 
+            // totalMessagesTextBox
+            // 
+            this.totalMessagesTextBox.Location = new System.Drawing.Point(553, 30);
+            this.totalMessagesTextBox.Name = "totalMessagesTextBox";
+            this.totalMessagesTextBox.Size = new System.Drawing.Size(100, 20);
+            this.totalMessagesTextBox.TabIndex = 7;
+            // 
+            // labelTotalMessages
+            // 
+            this.labelTotalMessages.Location = new System.Drawing.Point(553, 7);
+            this.labelTotalMessages.Name = "labelTotalMessages";
+            this.labelTotalMessages.Size = new System.Drawing.Size(100, 23);
+            this.labelTotalMessages.TabIndex = 9;
+            this.labelTotalMessages.Text = "Total Messages";
+            // 
+            // labelPassword
+            // 
+            this.labelPassword.Location = new System.Drawing.Point(264, 36);
+            this.labelPassword.Name = "labelPassword";
+            this.labelPassword.Size = new System.Drawing.Size(64, 23);
+            this.labelPassword.TabIndex = 8;
+            this.labelPassword.Text = "Password";
+            // 
+            // passwordTextBox
+            // 
+            this.passwordTextBox.Location = new System.Drawing.Point(328, 36);
+            this.passwordTextBox.Name = "passwordTextBox";
+            this.passwordTextBox.PasswordChar = '*';
+            this.passwordTextBox.Size = new System.Drawing.Size(128, 20);
+            this.passwordTextBox.TabIndex = 2;
+            this.passwordTextBox.Text = "muhammad";
+            // 
+            // labelUsername
+            // 
+            this.labelUsername.Location = new System.Drawing.Point(264, 5);
+            this.labelUsername.Name = "labelUsername";
+            this.labelUsername.Size = new System.Drawing.Size(64, 23);
+            this.labelUsername.TabIndex = 6;
+            this.labelUsername.Text = "Username";
+            // 
+            // loginTextBox
+            // 
+            this.loginTextBox.Location = new System.Drawing.Point(328, 5);
+            this.loginTextBox.Name = "loginTextBox";
+            this.loginTextBox.Size = new System.Drawing.Size(128, 20);
+            this.loginTextBox.TabIndex = 1;
+            this.loginTextBox.Text = "ekowibowo@swdevbali.com";
+            // 
+            // connectAndRetrieveButton
+            // 
+            this.connectAndRetrieveButton.Location = new System.Drawing.Point(460, 0);
+            this.connectAndRetrieveButton.Name = "connectAndRetrieveButton";
+            this.connectAndRetrieveButton.Size = new System.Drawing.Size(82, 39);
+            this.connectAndRetrieveButton.TabIndex = 5;
+            this.connectAndRetrieveButton.Text = "Connect and Retreive";
+            this.connectAndRetrieveButton.Click += new System.EventHandler(this.connectAndRetrieveButton_Click);
+            // 
+            // labelServerPort
+            // 
+            this.labelServerPort.Location = new System.Drawing.Point(97, 39);
+            this.labelServerPort.Name = "labelServerPort";
+            this.labelServerPort.Size = new System.Drawing.Size(31, 23);
+            this.labelServerPort.TabIndex = 3;
+            this.labelServerPort.Text = "Port";
+            // 
+            // portTextBox
+            // 
+            this.portTextBox.Location = new System.Drawing.Point(128, 39);
+            this.portTextBox.Name = "portTextBox";
+            this.portTextBox.Size = new System.Drawing.Size(128, 20);
+            this.portTextBox.TabIndex = 3;
+            this.portTextBox.Text = "110";
+            // 
+            // labelServerAddress
+            // 
+            this.labelServerAddress.Location = new System.Drawing.Point(16, 8);
+            this.labelServerAddress.Name = "labelServerAddress";
+            this.labelServerAddress.Size = new System.Drawing.Size(112, 23);
+            this.labelServerAddress.TabIndex = 1;
+            this.labelServerAddress.Text = "POP Server Address";
+            // 
+            // popServerTextBox
+            // 
+            this.popServerTextBox.Location = new System.Drawing.Point(128, 8);
+            this.popServerTextBox.Name = "popServerTextBox";
+            this.popServerTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Horizontal;
+            this.popServerTextBox.Size = new System.Drawing.Size(128, 20);
+            this.popServerTextBox.TabIndex = 0;
+            this.popServerTextBox.Text = "mail.swdevbali.com";
+            // 
+            // panelMiddle
+            // 
+            this.panelMiddle.Controls.Add(this.panelMessageBody);
+            this.panelMiddle.Controls.Add(this.panelMessagesView);
+            this.panelMiddle.Controls.Add(this.attachmentPanel);
+            this.panelMiddle.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelMiddle.Location = new System.Drawing.Point(0, 64);
+            this.panelMiddle.Name = "panelMiddle";
+            this.panelMiddle.Size = new System.Drawing.Size(806, 422);
+            this.panelMiddle.TabIndex = 3;
+            // 
+            // panelMessageBody
+            // 
+            this.panelMessageBody.Controls.Add(this.progressBar);
+            this.panelMessageBody.Controls.Add(this.messageTextBox);
+            this.panelMessageBody.Controls.Add(this.labelMessageBody);
+            this.panelMessageBody.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelMessageBody.Location = new System.Drawing.Point(281, 0);
+            this.panelMessageBody.Name = "panelMessageBody";
+            this.panelMessageBody.Size = new System.Drawing.Size(317, 422);
+            this.panelMessageBody.TabIndex = 6;
+            // 
+            // progressBar
+            // 
+            this.progressBar.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.progressBar.Location = new System.Drawing.Point(7, 398);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(301, 12);
+            this.progressBar.TabIndex = 10;
+            // 
+            // messageTextBox
+            // 
+            this.messageTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.messageTextBox.Location = new System.Drawing.Point(7, 22);
+            this.messageTextBox.MaxLength = 999999999;
+            this.messageTextBox.Multiline = true;
+            this.messageTextBox.Name = "messageTextBox";
+            this.messageTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.messageTextBox.Size = new System.Drawing.Size(301, 369);
+            this.messageTextBox.TabIndex = 9;
+            // 
+            // labelMessageBody
+            // 
+            this.labelMessageBody.Location = new System.Drawing.Point(8, 8);
+            this.labelMessageBody.Name = "labelMessageBody";
+            this.labelMessageBody.Size = new System.Drawing.Size(136, 16);
+            this.labelMessageBody.TabIndex = 5;
+            this.labelMessageBody.Text = "Message Body";
+            // 
+            // panelMessagesView
+            // 
+            this.panelMessagesView.Controls.Add(this.listMessages);
+            this.panelMessagesView.Controls.Add(this.labelMessageNumber);
+            this.panelMessagesView.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panelMessagesView.Location = new System.Drawing.Point(0, 0);
+            this.panelMessagesView.Name = "panelMessagesView";
+            this.panelMessagesView.Size = new System.Drawing.Size(281, 422);
+            this.panelMessagesView.TabIndex = 5;
+            // 
+            // listMessages
+            // 
+            this.listMessages.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listMessages.Location = new System.Drawing.Point(8, 24);
+            this.listMessages.Name = "listMessages";
+            this.listMessages.Size = new System.Drawing.Size(266, 386);
+            this.listMessages.TabIndex = 8;
+            // 
+            // labelMessageNumber
+            // 
+            this.labelMessageNumber.Location = new System.Drawing.Point(8, 8);
+            this.labelMessageNumber.Name = "labelMessageNumber";
+            this.labelMessageNumber.Size = new System.Drawing.Size(136, 16);
+            this.labelMessageNumber.TabIndex = 1;
+            this.labelMessageNumber.Text = "Messages";
+            // 
+            // attachmentPanel
+            // 
+            this.attachmentPanel.Controls.Add(this.listAttachments);
+            this.attachmentPanel.Controls.Add(this.labelAttachments);
+            this.attachmentPanel.Dock = System.Windows.Forms.DockStyle.Right;
+            this.attachmentPanel.Location = new System.Drawing.Point(598, 0);
+            this.attachmentPanel.Name = "attachmentPanel";
+            this.attachmentPanel.Size = new System.Drawing.Size(208, 422);
+            this.attachmentPanel.TabIndex = 4;
+            this.attachmentPanel.Visible = false;
+            // 
+            // listAttachments
+            // 
+            this.listAttachments.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listAttachments.Location = new System.Drawing.Point(8, 24);
+            this.listAttachments.Name = "listAttachments";
+            this.listAttachments.ShowLines = false;
+            this.listAttachments.ShowRootLines = false;
+            this.listAttachments.Size = new System.Drawing.Size(192, 386);
+            this.listAttachments.TabIndex = 10;
+            // 
+            // labelAttachments
+            // 
+            this.labelAttachments.Location = new System.Drawing.Point(12, 8);
+            this.labelAttachments.Name = "labelAttachments";
+            this.labelAttachments.Size = new System.Drawing.Size(136, 16);
+            this.labelAttachments.TabIndex = 3;
+            this.labelAttachments.Text = "Attachments";
+            // 
+            // panelProperties
+            // 
+            this.panelProperties.Controls.Add(this.gridHeaders);
+            this.panelProperties.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panelProperties.Location = new System.Drawing.Point(0, 302);
+            this.panelProperties.Name = "panelProperties";
+            this.panelProperties.Size = new System.Drawing.Size(806, 184);
+            this.panelProperties.TabIndex = 4;
+            // 
+            // gridHeaders
+            // 
+            this.gridHeaders.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.gridHeaders.DataMember = "";
+            this.gridHeaders.HeaderForeColor = System.Drawing.SystemColors.ControlText;
+            this.gridHeaders.Location = new System.Drawing.Point(0, 0);
+            this.gridHeaders.Name = "gridHeaders";
+            this.gridHeaders.PreferredColumnWidth = 400;
+            this.gridHeaders.ReadOnly = true;
+            this.gridHeaders.Size = new System.Drawing.Size(806, 188);
+            this.gridHeaders.TabIndex = 3;
             // 
             // SupportAlerter
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(507, 341);
+            this.ClientSize = new System.Drawing.Size(806, 486);
+            this.Controls.Add(this.panelProperties);
+            this.Controls.Add(this.panelMiddle);
+            this.Controls.Add(this.panelTop);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "SupportAlerter";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Support Alerter";
+            this.panelTop.ResumeLayout(false);
+            this.panelTop.PerformLayout();
+            this.panelMiddle.ResumeLayout(false);
+            this.panelMessageBody.ResumeLayout(false);
+            this.panelMessageBody.PerformLayout();
+            this.panelMessagesView.ResumeLayout(false);
+            this.attachmentPanel.ResumeLayout(false);
+            this.panelProperties.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.gridHeaders)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panelTop;
+        private System.Windows.Forms.CheckBox useSslCheckBox;
+        private System.Windows.Forms.Button uidlButton;
+        private System.Windows.Forms.TextBox totalMessagesTextBox;
+        private System.Windows.Forms.Label labelTotalMessages;
+        private System.Windows.Forms.Label labelPassword;
+        private System.Windows.Forms.TextBox passwordTextBox;
+        private System.Windows.Forms.Label labelUsername;
+        private System.Windows.Forms.TextBox loginTextBox;
+        private System.Windows.Forms.Button connectAndRetrieveButton;
+        private System.Windows.Forms.Label labelServerPort;
+        private System.Windows.Forms.TextBox portTextBox;
+        private System.Windows.Forms.Label labelServerAddress;
+        private System.Windows.Forms.TextBox popServerTextBox;
+        private System.Windows.Forms.Panel panelMiddle;
+        private System.Windows.Forms.Panel panelMessageBody;
+        private System.Windows.Forms.ProgressBar progressBar;
+        private System.Windows.Forms.TextBox messageTextBox;
+        private System.Windows.Forms.Label labelMessageBody;
+        private System.Windows.Forms.Panel panelMessagesView;
+        private System.Windows.Forms.TreeView listMessages;
+        private System.Windows.Forms.Label labelMessageNumber;
+        private System.Windows.Forms.Panel attachmentPanel;
+        private System.Windows.Forms.TreeView listAttachments;
+        private System.Windows.Forms.Label labelAttachments;
+        private System.Windows.Forms.Panel panelProperties;
+        private System.Windows.Forms.DataGrid gridHeaders;
     }
 }
 
--- SupportAlerter/SupportAlerter.cs
+++ SupportAlerter/SupportAlerter.cs
@@ -6,14 +6,134 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
-
+using OpenPop.Mime;
+using OpenPop.Mime.Header;
+using OpenPop.Pop3;
+using OpenPop.Pop3.Exceptions;
+using OpenPop.Common.Logging;
+using Message = OpenPop.Mime.Message;
 namespace SupportAlerter
 {
     public partial class SupportAlerter : Form
     {
+        private readonly Pop3Client pop3Client;
+        private readonly Dictionary<int, Message> messages = new Dictionary<int, Message>();
         public SupportAlerter()
         {
+            pop3Client = new Pop3Client();
+
             InitializeComponent();
         }
+
+        private void connectAndRetrieveButton_Click(object sender, EventArgs e)
+        {
+            ReceiveMails();
+        }
+
+        /*helper*/
+        private void ReceiveMails()
+        {
+            // Disable buttons while working
+            connectAndRetrieveButton.Enabled = false;
+            uidlButton.Enabled = false;
+            progressBar.Value = 0;
+
+            try
+            {
+                if (pop3Client.Connected)
+                    pop3Client.Disconnect();
+                pop3Client.Connect(popServerTextBox.Text, int.Parse(portTextBox.Text), useSslCheckBox.Checked);
+                pop3Client.Authenticate(loginTextBox.Text, passwordTextBox.Text);
+                int count = pop3Client.GetMessageCount();
+                totalMessagesTextBox.Text = count.ToString();
+                messageTextBox.Text = "";
+                messages.Clear();
+                listMessages.Nodes.Clear();
+                listAttachments.Nodes.Clear();
+
+                int success = 0;
+                int fail = 0;
+                for (int i = count; i >= 1; i -= 1)
+                {
+                    // Check if the form is closed while we are working. If so, abort
+                    if (IsDisposed)
+                        return;
+
+                    // Refresh the form while fetching emails
+                    // This will fix the "Application is not responding" problem
+                    Application.DoEvents();
+
+                    try
+                    {
+                        Message message = pop3Client.GetMessage(i);
+
+                        // Add the message to the dictionary from the messageNumber to the Message
+                        messages.Add(i, message);
+
+                        // Create a TreeNode tree that mimics the Message hierarchy
+                        TreeNode node = new TreeNodeBuilder().VisitMessage(message);
+
+                        // Set the Tag property to the messageNumber
+                        // We can use this to find the Message again later
+                        node.Tag = i;
+
+                        // Show the built node in our list of messages
+                        listMessages.Nodes.Add(node);
+
+                        success++;
+                    }
+                    catch (Exception e)
+                    {
+                        DefaultLogger.Log.LogError(
+                            "TestForm: Message fetching failed: " + e.Message + "\r\n" +
+                            "Stack trace:\r\n" +
+                            e.StackTrace);
+                        fail++;
+                    }
+
+                    progressBar.Value = (int)(((double)(count - i) / count) * 100);
+                }
+
+                MessageBox.Show(this, "Mail received!\nSuccesses: " + success + "\nFailed: " + fail, "Message fetching done");
+
+                if (fail > 0)
+                {
+                    MessageBox.Show(this,
+                                    "Since some of the emails were not parsed correctly (exceptions were thrown)\r\n" +
+                                    "please consider sending your log file to the developer for fixing.\r\n" +
+                                    "If you are able to include any extra information, please do so.",
+                                    "Help improve OpenPop!");
+                }
+            }
+            catch (InvalidLoginException)
+            {
+                MessageBox.Show(this, "The server did not accept the user credentials!", "POP3 Server Authentication");
+            }
+            catch (PopServerNotFoundException)
+            {
+                MessageBox.Show(this, "The server could not be found", "POP3 Retrieval");
+            }
+            catch (PopServerLockedException)
+            {
+                MessageBox.Show(this, "The mailbox is locked. It might be in use or under maintenance. Are you connected elsewhere?", "POP3 Account Locked");
+            }
+            catch (LoginDelayException)
+            {
+                MessageBox.Show(this, "Login not allowed. Server enforces delay between logins. Have you connected recently?", "POP3 Account Login Delay");
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(this, "Error occurred retrieving mail. " + e.Message, "POP3 Retrieval");
+            }
+            finally
+            {
+                // Enable the buttons again
+                connectAndRetrieveButton.Enabled = true;
+                uidlButton.Enabled = true;
+                progressBar.Value = 100;
+            }
+        }
+
+		
     }
 }
