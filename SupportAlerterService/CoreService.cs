dcf96b1c083debbcc8b0d817f411c0b23a68accb
Eko Suprapto Wibowo <swdev.bali@gmail.com>
2013-01-23 17:09:30 +0700

    added timer feature

--- SupportAlerterService/CoreService.cs
+++ SupportAlerterService/CoreService.cs
@@ -7,11 +7,14 @@ using System.Linq;
 using System.ServiceProcess;
 using System.Text;
 using System.Threading.Tasks;
+using System.Timers;
 
 namespace SupportAlerterService
 {
     public partial class CoreService : ServiceBase
     {
+        private Timer _timer;
+        private static int _count = 0;
         public CoreService()
         {
             InitializeComponent();
@@ -20,11 +23,28 @@ namespace SupportAlerterService
         protected override void OnStart(string[] args)
         {
             EventLog.WriteEntry(Program.EventLogName, "The service was started successfully.", EventLogEntryType.Information);
+            _timer = new Timer(10 * 60 * 1000);// 1 minute
+            _timer.Elapsed += new System.Timers.ElapsedEventHandler(timer_Elapsed);
+            _timer.Start(); // <- important
         }
 
         protected override void OnStop()
         {
             EventLog.WriteEntry(Program.EventLogName, "The service was stopped successfully.", EventLogEntryType.Information);
+        }
+
+        private void timer_Elapsed(object sender, System.Timers.ElapsedEventArgs e)
+        {
+            _timer.Stop();
+            try
+            {
+                EventLog.WriteEntry(Program.EventLogName, "Checking emails " + _count++);
+            }
+            catch (Exception ex)
+            {
+                EventLog.WriteEntry(Program.EventLogName, "This is my error " + ex.Message);
+            }
+            _timer.Start();
         }
     }
 }
--- SupportAlerterService/Program.cs
+++ SupportAlerterService/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.ServiceProcess;
 using System.Text;
@@ -10,9 +11,30 @@ namespace SupportAlerterService
     static class Program
     {
         public const string EventLogName = "eMail To SMS";
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
+        private static object _lockObject = new object();
+        private static int _runningTasks = 0;
+
+        public static void PrintToConsole()
+        {
+            lock (_lockObject)
+            {
+                _runningTasks += 1;
+            }
+            try
+            {
+                EventLog.WriteEntry(Program.EventLogName, "Starting work. Total active: " + _runningTasks, EventLogEntryType.Information,1);
+                var r = new Random();
+                System.Threading.Thread.Sleep(r.Next(1000));//3500
+            }
+            finally
+            {
+                lock (_lockObject)
+                {
+                    _runningTasks -= 1;
+                }
+            }
+        }
+
         static void Main()
         {
             ServiceBase[] ServicesToRun;
