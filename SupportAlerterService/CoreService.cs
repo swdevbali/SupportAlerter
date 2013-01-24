8ad9b3ca2830e1785116f55bdc53767d009be30b
Eko Suprapto Wibowo <swdev.bali@gmail.com>
2013-01-24 09:20:49 +0700

    Revert "After adding DB connection, it won't start"
    This reverts commit 61ddfa4c1754e78908d1ff25a6d90705cf96f15d.

--- SupportAlerterService/CoreService.cs
+++ SupportAlerterService/CoreService.cs
@@ -1,9 +1,7 @@
-﻿using SupportAlerterLibrary;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
-using System.Data.SqlServerCe;
 using System.Diagnostics;
 using System.Linq;
 using System.ServiceProcess;
@@ -25,9 +23,9 @@ namespace SupportAlerterService
         protected override void OnStart(string[] args)
         {
             EventLog.WriteEntry(Program.EventLogName, "The service was started successfully.", EventLogEntryType.Information);
-            _timer = new Timer(/*10 * 60 */ 5 * 1000);// 1 minute
+            _timer = new Timer(/*10 * 60 */ 1000);// 1 minute
             _timer.Elapsed += new System.Timers.ElapsedEventHandler(timer_Elapsed);
-            //_timer.Start(); // <- important
+            _timer.Start(); // <- important
         }
 
         protected override void OnStop()
@@ -39,18 +37,15 @@ namespace SupportAlerterService
         {
             _timer.Stop();
             try
-            {
-                CoreFeature.getInstance().getDataConnection().Open();
-                SqlCeCommand cmd = CoreFeature.getInstance().getDataConnection().CreateCommand();
-                cmd.CommandText = "select server,port,use_ssl,username,password from account where active=true'";
-                cmd.CommandType = CommandType.Text;
-                SqlCeDataReader rdr = cmd.ExecuteReader();
-                while(rdr.Read())
+            {   
+                if (SupportAlerterLibrary.CoreFeature.TestConnection(false,"mail.swdevbali.com", 110, false, "ekowibowo@swdevbali.com", "muhammad"))
                 {
-                    if (SupportAlerterLibrary.CoreFeature.getInstance().TestConnection(false,"mail.swdevbali.com", 110, false, "ekowibowo@swdevbali.com", "muhammad"))
-                    {
-                        EventLog.WriteEntry(Program.EventLogName, "Login success : " + _count++);
-                    }
+                    EventLog.WriteEntry(Program.EventLogName, "Login success : " + _count++);
+                }
+
+                if (!SupportAlerterLibrary.CoreFeature.TestConnection(false, "xmail.swdevbali.com", 110, false, "ekowibowo@swdevbali.com", "muhammad"))
+                {
+                    EventLog.WriteEntry(Program.EventLogName, "Login success : " + _count++);
                 }
             }
             catch (Exception ex)
